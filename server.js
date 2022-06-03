5394674509ca0b4f0856c81b78810ff8f93746f3
Samantha Hughes <samantha.patricia.hughes2@gmail.com>
2022-06-03 16:40:34 -0400

    express & body-parser added

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/", function (req, res) {
+    res.send("express is working")
+})
+
+app.listen(3000, function () {
+    console.log("server is running on 3000");
+})
