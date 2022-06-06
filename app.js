382e4da7f4374a5d164af36e73d6da5f68c457a1
effdiggity <dear.francesca@gmail.com>
2022-06-06 13:38:58 -0400

    solved localhost stuff (hopefully forever) and restarting at 42:40

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const express = require("express");
 const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT || 8080;
 
 require('dotenv').config();
 
-app.use(express.urlencoded( { extended: true } ));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(expressLayouts);
 
-app.set('layout', '/layouts/main');
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
 
-const routes = require('layout', './server/routes/ribbonRoutes.js')
+const routes = require('./server/routes/ribbonRoutes.js')
 app.use('/', routes)
 
 app.listen(port, () => console.log(`server is running on ${port}`));
